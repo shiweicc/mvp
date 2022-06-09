efbbddea2f152a8d5fc5b252e7c25dce92489019
Shiwei Chen <shiweichenus@gmail.com>
2022-06-08 20:35:29 -0500

    Complete display categories

--- client/components/CategoryItem.js
+++ client/components/CategoryItem.js
@@ -4,6 +4,7 @@ import React from 'react';
 const CategoryItem = (props) => {
 
   console.log('here in item: ', props.item.strCategory);
+
   let category = props.item.strCategory;
 
   return (
--- client/components/CategoryList.js
+++ client/components/CategoryList.js
@@ -4,18 +4,15 @@ import CategoryItem from './CategoryItem';
 const CategoryList = (props) => {
   console.log('here in list: ', props.category);
 
-  // const listCategory = props.category.map((item) => {
-  //   <CategoryItem value={item.strCategory}/>
-  // })
+  const listCategory = props.category.map(item => (
+    <CategoryItem item={item} />
+  ))
 
   return (
     <div>
       <h3>Meals by categories: </h3>
       <ul>
-        {props.category.map(item => {
-          <CategoryItem item={item}/>
-        })}
-        {/* <CategoryItem /> */}
+        {listCategory}
       </ul>
     </div>
   );
